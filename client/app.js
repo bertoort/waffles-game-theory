bf524c7c847dca93f70ae901162e5027c0a0b92f
Roberto Ortega <berto.ort@gmail.com>
2023-04-17 16:55:52 +0900

    update for new deployment

--- client/app.js
+++ client/app.js
@@ -1,16 +1,16 @@
 // creating the websocket connection
-var socket = new WebSocket("wss://waffles-game-theory.herokuapp.com/ws");
+var socket = new WebSocket("wss://https://waffle-game-theory.onrender.com/ws");
 
 // creating a unique id for player to know and send
 var id = Date.now();
 
 // when an update is received via ws connection, we update the model
-socket.onmessage = function(evt){
+socket.onmessage = function (evt) {
   var data = JSON.parse(evt.data);
   if (data.reset) {
-    reset()
+    reset();
   }
-  interpretInfo(data)
+  interpretInfo(data);
   game.gameState = data;
 };
 
@@ -18,7 +18,7 @@ socket.onmessage = function(evt){
 function interpretInfo(data) {
   if (data.results.playerOne) {
     game.opponentReady = true;
-    resolveWinner(data)
+    resolveWinner(data);
   } else if (data.playerOne.status && !game.playerReady) {
     game.opponentReady = true;
   }
@@ -27,34 +27,34 @@ function interpretInfo(data) {
 // find and display winner
 function resolveWinner(data) {
   var same;
-  var playerOneResult = data.results.playerOne
-  var playerTwoResult = data.results.playerTwo
+  var playerOneResult = data.results.playerOne;
+  var playerTwoResult = data.results.playerTwo;
   if (playerOneResult == playerTwoResult) {
-    same = true
+    same = true;
     switch (playerOneResult) {
       case "split":
-        game.winnerMessage = "Waffles for everybody!!"
+        game.winnerMessage = "Waffles for everybody!!";
         break;
       case "take":
-        game.winnerMessage = "Oh no!! The waffle is trashed!"
+        game.winnerMessage = "Oh no!! The waffle is trashed!";
     }
   }
   if (id == data.playerOne.id) {
     if (!same && data.results.playerOne == "split") {
-      game.winnerMessage = "Your friend ate the waffle"
+      game.winnerMessage = "Your friend ate the waffle";
     } else if (!same && data.results.playerOne == "take") {
-      game.winnerMessage = "You ate the whole waffle!"
+      game.winnerMessage = "You ate the whole waffle!";
     }
-    game.playerChoice = data.results.playerOne
-    game.opponentChoice = data.results.playerTwo
+    game.playerChoice = data.results.playerOne;
+    game.opponentChoice = data.results.playerTwo;
   } else {
     if (!same && data.results.playerTwo == "split") {
-      game.winnerMessage = "Your friend took ate the waffle"
+      game.winnerMessage = "Your friend took ate the waffle";
     } else if (!same && data.results.playerTwo == "take") {
-      game.winnerMessage = "You eat the whole waffle!"
+      game.winnerMessage = "You eat the whole waffle!";
     }
-    game.playerChoice = data.results.playerTwo
-    game.opponentChoice = data.results.playerOne
+    game.playerChoice = data.results.playerTwo;
+    game.opponentChoice = data.results.playerOne;
   }
 }
 
@@ -71,43 +71,51 @@ function reset() {
 Vue.config.debug = true; //TODO: Remove in production
 
 // transistions
-Vue.transition('board', {
-    enterClass: 'bounceInDown',
-    leaveClass: 'bounceOutDown'
+Vue.transition("board", {
+  enterClass: "bounceInDown",
+  leaveClass: "bounceOutDown",
 });
 
 // creating the vue instance to send information
 var game = new Vue({
-    el: '#game',
-    data: {
-        gameState: {
-            started: false,
-            messages: [],
-        },
-        text: "",
-        choice: "",
-        playerReady: "",
-        opponentReady: "",
-        winnerMessage: "",
-        playerChoice: "",
-        opponentChoice: "",
+  el: "#game",
+  data: {
+    gameState: {
+      started: false,
+      messages: [],
     },
-    methods: {
-        submit: function(){
-          this.playerReady = true;
-          var message = JSON.stringify({type: "submission", data: this.choice, id: id})
-          socket.send(message);
-        },
-        message: function () {
-          if (this.text != "") {
-            var message = JSON.stringify({type: "message", data: this.text, id: id})
-            socket.send(message)
-            this.text = "";
-          }
-        },
-        reset: function () {
-          var message = JSON.stringify({type: "command", data: "reset", id: id})
-          socket.send(message)
-        }
-    }
+    text: "",
+    choice: "",
+    playerReady: "",
+    opponentReady: "",
+    winnerMessage: "",
+    playerChoice: "",
+    opponentChoice: "",
+  },
+  methods: {
+    submit: function () {
+      this.playerReady = true;
+      var message = JSON.stringify({
+        type: "submission",
+        data: this.choice,
+        id: id,
+      });
+      socket.send(message);
+    },
+    message: function () {
+      if (this.text != "") {
+        var message = JSON.stringify({
+          type: "message",
+          data: this.text,
+          id: id,
+        });
+        socket.send(message);
+        this.text = "";
+      }
+    },
+    reset: function () {
+      var message = JSON.stringify({ type: "command", data: "reset", id: id });
+      socket.send(message);
+    },
+  },
 });
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bertoort/game-theory
+
+go 1.17
+
+require github.com/gorilla/websocket v0.0.0-20160128164856-234959944d9c
